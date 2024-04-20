224d76a346e78a3af33879ee1f7482166220c96f
TShreek <124446964+TShreek@users.noreply.github.com>
2024-04-21 00:24:35 +0530

    Update 69.c
    remove unnecessary lines and make it understandable

--- Sem4/leetCode/ArraysStacks/69.c
+++ Sem4/leetCode/ArraysStacks/69.c
@@ -1,20 +1,5 @@
-/* my code
-  int mySqrt(int x) {
-    int n;
-    int i=1;
-    while(x%i == 0)
-    {
-        if(x/i == i)
-            return i;
-        i++;
-    }
-    i=1;
-        while(i*i <= x)
-              i++;
-    return i-1;
-           
-}*/
-// Actual soln :
+//Testing for 2 cases
+// If divsible by the number ie perfect square
 int mySqrt(int x) {
     int n;
     int i=1;
@@ -24,6 +9,7 @@ int mySqrt(int x) {
             return i;
         i++;
     }
+  //If not perfect square :
     i=1;
         while(i*i <= x)
               i++;
