56637f621437be295e088dbc38ce3982e3410999
TShreek <124446964+TShreek@users.noreply.github.com>
2024-04-21 22:14:05 +0530

    Update 35.c

--- Sem4/leetCode/ArraysStacks/35.c
+++ Sem4/leetCode/ArraysStacks/35.c
@@ -1,4 +1,5 @@
-//Similar to binary search but with 3 inputs
+//Similar to binary search but with 3 inputs instead of high and low as practiced before
+//Search for an element using Binary search and return the position where the element shoudlve been if it was present incase it isnt present
 int searchInsert(int* nums, int numsSize, int target) {
     int low = 0;
     int high = numsSize - 1;
