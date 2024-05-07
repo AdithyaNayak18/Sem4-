2a0fd74b870b68b9d549e430e57daff1a8d8fe85
TShreek <124446964+TShreek@users.noreply.github.com>
2024-05-07 23:12:42 +0530

    Update BoatsWeightProblem.js

--- Sem4/leetCode/ProbOfTheDays/BoatsWeightProblem.js
+++ Sem4/leetCode/ProbOfTheDays/BoatsWeightProblem.js
@@ -1,8 +1,10 @@
 /**
+Given weight of people ex: [4,2,1,5,2], we must board them on a boat such that it doesnt exceed Limit Weight
  * @param {number[]} people
  * @param {number} limit
  * @return {number}
  */
+
 var numRescueBoats = function(people, limit) {
     people.sort((a,b)=>a-b);
     var boats=0;
