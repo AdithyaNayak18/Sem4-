f5627428338234d504e63162ab31e9e40775316a
TShreek <124446964+TShreek@users.noreply.github.com>
2024-05-23 22:00:40 +0530

    Create searchRotatedArray.py

---
+++ Sem4/leetCode/ArraysStacks/searchRotatedArray.py
@@ -0,0 +1,30 @@
+"Refer C code for explaination 
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        length = len(nums)
+        low = 0
+        high = length - 1
+        
+        while low <= high:
+            mid = low + (high - low) // 2
+            
+            if nums[mid] == target:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        
+        return -1
