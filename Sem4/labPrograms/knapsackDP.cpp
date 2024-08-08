053132dcb279468faaf44ff47cfa5e130d73a77e
Shreekrishna T <124446964+TShreek@users.noreply.github.com>
2024-08-08 22:55:53 +0530

    Update knapsackDP.cpp

--- Sem4/labPrograms/knapsackDP.cpp
+++ Sem4/labPrograms/knapsackDP.cpp
@@ -1,41 +1,48 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-void knapsack(int wt, int wts[], int prof[], int n, vector<pair<int,int>>& included) {
-    int dp[n + 1][wt + 1];
-
-    for (int i = 0; i <= n; ++i) {
-        for (int j = 0; j <= wt; ++j) {
-            if (i == 0 || j == 0)
-                dp[i][j] = 0;
-            else if (wts[i - 1] <= j) {
-                int include = prof[i - 1] + dp[i - 1][j - wts[i - 1]];
-                int exclude = dp[i - 1][j];
-                if (include > exclude) {
-                    dp[i][j] = include;
-                    included.push_back(make_pair(i - 1, j - wts[i - 1]));
-                } else {
-                    dp[i][j] = exclude;
-                }
+// Function to solve the Knapsack problem using DP
+void knapsack(int W, const vector<int>& weights, const vector<int>& values, int n) {
+    
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+    
+    for (int i = 1; i <= n; ++i) {
+        for (int w = 1; w <= W; ++w) {
+            if (weights[i - 1] <= w) {
+                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1]);
             } else {
-                dp[i][j] = dp[i - 1][j];
+                dp[i][w] = dp[i - 1][w];
             }
         }
+    }  
+    // Printing the maximum value
+    cout << "Maximum value in Knapsack = " << dp[n][W] << endl;  
+    // Tracking which items are included
+    vector<bool> included(n, false);
+    int w = W;
+    for (int i = n; i > 0 && dp[i][w] > 0; --i) {
+        if (dp[i][w] != dp[i - 1][w]) {
+            included[i - 1] = true;
+            w -= weights[i - 1];
+        }
+    } 
+    // Displaying the selected items
+    cout << "Items included in the knapsack:" << endl;
+    for (int i = 0; i < n; ++i) {
+        if (included[i]) {
+            cout << "Item " << i + 1 << " (Weight: " << weights[i] << ", Value: " << values[i] << ")" << endl;
+        }
     }
-
-    cout << "value of items included in the knapsack:" << endl;
-    cout << dp[n][wt];
 }
 
 int main() {
-    int maxWeight = 300;
-    int weights[] = {110, 140, 150, 130};
-    int profits[] = {20, 18, 19, 25};
-    int n = sizeof(weights) / sizeof(weights[0]);
+    int W = 50; 
+    vector<int> weights = {10, 20, 30}; 
+    vector<int> values = {60, 100, 120}; 
+    int n = weights.size(); 
 
-    vector<pair<int,int>> included;
-    knapsack(maxWeight, weights, profits, n, included);
-
+    knapsack(W, weights, values, n);
     return 0;
 }
